567d8205fdf1c07f415f000433cee6213df4efcb
wangyu <wangyu@homelabs.cn>
2021-12-21 11:08:33 +0800

    update index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+const { default: axios } = require("axios");
 
+function testRequest() {
+    axios.get('https://autumnfish.cn/search?keywords=Love%20Story').then(res => {
+        console.log('request success')
+        console.log(res)
+    }
+    ).catch(error => {
+        console.log('request failed')
+        console.log(`error${error}`)
+    })
+}
 
-console.log('log in index.js')
+testRequest()
